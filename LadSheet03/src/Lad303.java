bb7ad20c9003cca13d47f8afb300c4380e6208c6
AhuttenC <Attakorn9420@gmail.com>
2024-12-11 11:16:11 +0700

    LadSheet03[Lad303]

---
+++ LadSheet03/src/Lad303.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Lad303 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input a sentence : ");
+		String sentence = scan.next();
+		final char SPACEBER = ' ';
+		while (true) {
+			if (sentence.endsWith(".")) { break; }
+			System.out.print("Input a sentence, again : ");
+			sentence = scan.next();
+		}
+		for (int i = 0; i < sentence.length(); i++) {
+			char sentencech = sentence.charAt(i);
+			if (sentencech == SPACEBER) {
+				if (
+			
+		}
+	}
+}
